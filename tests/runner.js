a209b3f21538f19d9cfe4311d1e146067513fa00
gzip <gamaiel@yahoo.com>
2013-08-05 12:55:47 -0700

    Consolidate runner code

---
+++ tests/runner.js
@@ -0,0 +1,46 @@
+var status = 0,
+    page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+    // exit once total has been logged
+    if (msg.match('TOTAL: ')) {
+        phantom.exit(status);
+    }
+    // change status to non-zero if fail was logged
+    if (msg.match('FAIL: ')) {
+        status = 1;
+    }
+}
+
+page.onLoadFinished = function(status) {
+    if (status !== 'success') {
+        console.log('Unable to open test html');
+        phantom.exit(1);
+    } else {
+        page.evaluate(function() {
+            console.log('Starting tests...');
+
+            var passed = 0,
+                failed = 0,
+                runner = mocha.run();
+
+            runner.on('pass', function(test) {
+                passed++;
+                console.log('PASS: ' + test.fullTitle());
+            });
+
+            runner.on('fail', function(test, err) {
+                failed++;
+                console.log('FAIL: ' + test.fullTitle() + ':: ' + err.message);
+            });
+
+            runner.on('end', function() {
+                console.log('TOTAL: ' + (passed + failed) + ', PASSED: ' + passed + ', FAILED: ' + failed);
+            });
+        });
+    }
+};
+
+page.open('./index.html');
+
