f189ce371809a00823dc3cf9d9bc29d70a1f8a5a
gzip <gamaiel@yahoo.com>
2013-08-05 12:31:13 -0700

    Set up mocha test failure to fail the build

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,24 @@
-var page = require('webpage').create();
-page.open('./index.html', function (status) {
+var status = 0,
+    page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+    // exit once total has been logged
+    if (msg.match('TOTAL')) {
+        phantom.exit(status);
+    }
+    // change status to non-zero if fail was logged
+    if (msg.match('FAIL')) {
+        status = 1;
+    }
+}
+
+page.onLoadFinished = function(status) {
     if (status !== 'success') {
         console.log('Unable to open test html');
-    } else {
-        //console.log(page.content);
+        phantom.exit(1);
     }
-    phantom.exit();
-});
+};
+
+page.open('./index.html');
+
