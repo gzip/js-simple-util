eec218599e9f2da325a23c686dc71cbb9770c912
gzip <gamaiel@yahoo.com>
2013-08-04 22:42:51 -0700

    Add test scaffolding

---
+++ tests/index.js
@@ -0,0 +1,9 @@
+var page = require('webpage').create();
+page.open('./index.html', function (status) {
+    if (status !== 'success') {
+        console.log('Unable to open test html');
+    } else {
+        console.log(page.content);
+    }
+    phantom.exit();
+});
